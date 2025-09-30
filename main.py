ede5989f3674f4e3285a021ef17a4e0081243f8e
Atahan500 <atahanmurza62@gmail.com>
2025-09-30 19:58:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+meme_dictonary = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+
+
+word = input("Anlamadığınız bir kelime yazın hepsi büyük harf olacak")
+
+if word in meme_dictonary.keys():
+    meaning = meme_dictonary[word]
+    print('Anlamı:',meaning)
+else:
+    print("Malesef  bu  sözlükte kelime yok.")
+
 
